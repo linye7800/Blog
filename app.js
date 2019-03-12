f67c96bf4d967a005b107740c776b8df1fb2f0c1
ye.lin <linye102@gmail.com>
2019-03-12 11:21:49 +0800

    update .gitignore

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 /**
  * Created by Lincoln on 2019-03-12.
+ *
+ * app (entrance) start
  */
+
+var express = require('express');
+var swig = require('swig');
+
+// app => http services
+var app = express();
+
+//static files
+app.use('/public', express.static( __dirname + '/public'));
+
+// Front-end Back-end
+app.engine('html', swig.renderFile);
+
+app.set('views', './views');
+
+app.set('view engine', 'html');
+
+swig.setDefaults({cache: false});
+
+app.get('/', function (req, res) {
+   res.render('index');
+});
+
+app.listen('3000');
